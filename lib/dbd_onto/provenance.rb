3507e71a3fbb1bfee509a03be6eccc75a5cce828
Peter Vandenabeele <peter@vandenabeele.com>
2013-05-23 21:02:10 +0200

    First provenance property prov:context stored in Dbd
    * using Dbd::Graph to store ontologies for Dbd
    * predicate (rdf:predicate) is prov:context
    * label (from RDF::RDFS.label) is Context

--- lib/dbd_onto/provenance.rb
+++ lib/dbd_onto/provenance.rb
@@ -1,4 +1,29 @@
 module DbdOnto
   class Provenance < Base
+
+    def initialize
+      super
+      self << provenance
+      self << resource
+    end
+
+  private
+
+    def provenance
+      @provenance ||= Dbd::ProvenanceResource.new
+    end
+
+    def resource
+      @resource ||= Dbd::Resource.new(provenance_subject: provenance.subject)
+      context_predicate = Dbd::Fact.new(
+        predicate: 'rdf:predicate',
+        object: 'prov:context')
+      context_label = Dbd::Fact.new(
+        predicate: RDF::RDFS.label.qname.join(':'),
+        object: 'Context')
+      @resource << context_predicate
+      @resource << context_label
+    end
+
   end
 end
--- spec/lib/dbd_onto/provenance_spec.rb
+++ spec/lib/dbd_onto/provenance_spec.rb
@@ -6,4 +6,21 @@ describe DbdOnto::Provenance do
     subject.should be_a(Dbd::Graph)
   end
 
+  describe "properties include" do
+    describe "context" do
+      it "predicate is prov:context" do
+        subject.detect do |fact|
+          fact.predicate == 'rdf:predicate' &&
+          fact.object == 'prov:context'
+        end.should_not be_nil
+      end
+
+      it "label is Context" do
+        subject.detect do |fact|
+          fact.predicate == 'rdfs:label' &&
+          fact.object == 'Context'
+        end.should_not be_nil
+      end
+    end
+  end
 end
