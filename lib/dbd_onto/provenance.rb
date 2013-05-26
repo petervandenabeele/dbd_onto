4d31a4972ed37f329e7a9883bf48baa7f518b847
Peter Vandenabeele <peter@vandenabeele.com>
2013-05-26 21:03:59 +0200

    Add more provenance attributes and provenance info
    * generate different resources per provenance attribute
    * add prov:source, prov:license
    * add dcterms:creator and dcterms:created
    * add provenance info

--- lib/dbd_onto/provenance.rb
+++ lib/dbd_onto/provenance.rb
@@ -1,38 +1,76 @@
 module DbdOnto
   class Provenance < Base
 
+    PROV_ATTRIBUTES = %w(context source license)
+    DCTERMS_ATTRIBUTES = %w(creator created)
+
     def initialize
       super
       self << provenance
-      self << resource
+      add_prov_attributes
+      add_dcterms_attributes
     end
 
   private
 
     def provenance
-      return @provenance if @provenance
-      @provenance = Dbd::ProvenanceResource.new
-      @provenance << provenance_fact_context_public
+      # memoize to avoid making new_provenance on each call
+      @provenance ||= new_provenance
     end
 
-    def resource
-      return @resource if @resource
-      @resource = Dbd::Resource.new(provenance_subject: provenance.subject)
-      @resource << fact_defines_predicate_prov_context
-      @resource << fact_label_context
+    PROVENANCE = [
+      ['prov:context', 'public'],
+      ['prov:source' , 'https://github.com/petervandenabeele/dbd/blob/d37360070e7f8e61a19c2bca210c881a151ded75/docs/rationale.md'],
+      ['dcterms:creator' , 'Peter Vandenabeele (@peter_v)'],
+      ['dcterms:created', Time.new(2013,5,26,21,30,0).utc],
+      ['prov:license', 'Copyright 2013 Peter Vandenabeele (http://vandenabeele.com), licensed under MIT license.']
+    ]
+
+    def new_provenance
+      Dbd::ProvenanceResource.new.tap do |_provenance|
+        PROVENANCE.each do |provenance_fact_data|
+          _provenance << provenance_fact(provenance_fact_data)
+        end
+      end
     end
 
-    def provenance_fact_context_public
-      Dbd::ProvenanceFact.new(predicate: 'prov:context', object: 'public')
+    def provenance_fact(provenance_fact_data)
+      Dbd::ProvenanceFact.new(
+        predicate: provenance_fact_data.first,
+        object: provenance_fact_data.last)
     end
 
-    def fact_defines_predicate_prov_context
-      Dbd::Fact.new(predicate: 'meta:defines_predicate', object: 'prov:context')
+    def add_prov_attributes
+      PROV_ATTRIBUTES.each do |attribute|
+        resource = Dbd::Resource.new(provenance_subject: provenance.subject)
+        resource << fact_defines_predicate_prov(attribute)
+        resource << fact_label(attribute)
+        self << resource
+      end
     end
 
-    def fact_label_context
+    # NOTE using dcterms:creator is not exact for now as the range
+    #      should be a dcterms:Agent and it is a Literal here.
+    def add_dcterms_attributes
+      DCTERMS_ATTRIBUTES.each do |attribute|
+        resource = Dbd::Resource.new(provenance_subject: provenance.subject)
+        resource << fact_defines_predicate_dcterms(attribute)
+        resource << fact_label(attribute)
+        self << resource
+      end
+    end
+
+    def fact_defines_predicate_prov(attribute)
+      Dbd::Fact.new(predicate: 'meta:defines_predicate', object: "prov:#{attribute}")
+    end
+
+    def fact_defines_predicate_dcterms(attribute)
+      Dbd::Fact.new(predicate: 'meta:defines_predicate', object: "dcterms:#{attribute}")
+    end
+
+    def fact_label(attribute)
       # TODO in Dbd : turn predicate into a proper Predicate class
-      Dbd::Fact.new(predicate: RDF::RDFS.label.qname.join(':'), object: 'Context')
+      Dbd::Fact.new(predicate: RDF::RDFS.label.qname.join(':'), object: attribute.capitalize)
     end
 
   end
--- spec/lib/dbd_onto/provenance_spec.rb
+++ spec/lib/dbd_onto/provenance_spec.rb
@@ -9,24 +9,95 @@ describe DbdOnto::Provenance do
   it "all facts in the provenance ontology have a provenance with context public" do
     subject.all? do |fact|
       fact.is_a?(Dbd::ProvenanceFact) ||
-        subject.by_subject(fact.provenance_subject).
-          detect {|p| p.predicate == 'prov:context' && p.object == 'public'}
+        check_provenance?(subject.by_subject(fact.provenance_subject))
     end.should be_true
   end
 
+  def check_provenance?(provenance_facts)
+    provenance_facts.detect {|p| p.predicate == 'prov:context' && p.object == 'public'} &&
+    provenance_facts.detect {|p| p.predicate == 'prov:source'} &&
+    provenance_facts.detect {|p| p.predicate == 'dcterms:creator'} &&
+    provenance_facts.detect {|p| p.predicate == 'dcterms:created'} &&
+    provenance_facts.detect {|p| p.predicate == 'prov:license'}
+  end
+
   describe "properties include" do
-    describe "context" do
+
+    let(:context_subject) do
+      subject.detect do |fact|
+        fact.predicate == 'meta:defines_predicate' &&
+        fact.object == 'prov:context'
+      end.subject
+    end
+
+    let(:source_subject) do
+      subject.detect do |fact|
+        fact.predicate == 'meta:defines_predicate' &&
+        fact.object == 'prov:source'
+      end.subject
+    end
+
+    describe "prov:subject and prov:source" do
+      it "are different subjects" do
+        context_subject.should_not == source_subject
+      end
+    end
+
+    describe "prov:context" do
+
+      let(:context_facts) { subject.by_subject(context_subject) }
+
       it "defines the predicate prov:context" do
-        subject.detect do |fact|
-          fact.predicate == 'meta:defines_predicate' &&
-          fact.object == 'prov:context'
-        end.should_not be_nil
+        context_subject.should_not be_nil
       end
 
-      it "label is Context" do
-        subject.detect do |fact|
+      it "has label Context" do
+        context_facts.detect do |fact|
           fact.predicate == 'rdfs:label' &&
           fact.object == 'Context'
+        end.should_not be_nil
+      end
+    end
+
+    describe "prov:source" do
+
+      let(:source_facts) { subject.by_subject(source_subject) }
+
+      it "defines the predicate prov:source" do
+        source_subject.should_not be_nil
+      end
+
+      it "has label Source" do
+        source_facts.detect do |fact|
+          fact.predicate == 'rdfs:label' &&
+          fact.object == 'Source'
+        end.should_not be_nil
+      end
+    end
+
+    describe "dcterms:creator" do
+      it "defines the predicate" do
+        subject.detect do |fact|
+          fact.predicate == 'meta:defines_predicate' &&
+          fact.object == 'dcterms:creator'
+        end.should_not be_nil
+      end
+    end
+
+    describe "dcterms:created" do
+      it "defines the predicate" do
+        subject.detect do |fact|
+          fact.predicate == 'meta:defines_predicate' &&
+          fact.object == 'dcterms:created'
+        end.should_not be_nil
+      end
+    end
+
+    describe "prov:license" do
+      it "defines the predicate" do
+        subject.detect do |fact|
+          fact.predicate == 'meta:defines_predicate' &&
+          fact.object == 'prov:license'
         end.should_not be_nil
       end
     end
