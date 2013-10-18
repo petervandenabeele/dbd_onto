878388b827f3e5678858eb89af6dbbe141cde7d9
Peter Vandenabeele <peter@vandenabeele.com>
2013-10-18 22:06:44 +0200

    Add filtering to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,8 @@ Dir[root + '/support/**/*.rb'].each {|f| require f}
 
 RSpec.configure do |config|
   config.order = 'random'
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
