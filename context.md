204f98ee3f89ce78f62cbd1a69f52fe5f66a3f2b
Peter Vandenabeele <peter@vandenabeele.com>
2013-10-20 15:20:58 +0200

    Add URI and Comment to context ontology

---
+++ context.md
@@ -0,0 +1,10 @@
+# Context ontology
+
+First a limited context ontology is constructed. It defines (or reuses) these predicates:
+
+* context:visibility
+* context:encryption
+* context:license
+* dc:source
+* dc:creator
+* dcterms:created
--- spec/lib/dbd_onto/context_spec.rb
+++ spec/lib/dbd_onto/context_spec.rb
@@ -10,8 +10,10 @@ describe DbdOnto::Context do
 
   it 'all facts in the context ontology have the meta context as context' do
     subject.all? do |fact|
-      fact.is_a?(Dbd::ContextFact) ||
+      begin
+        fact.is_a?(Dbd::ContextFact) ||
         check_context?(subject.by_subject(fact.context_subject))
+      end
     end.should be_true
   end
 
@@ -19,22 +21,57 @@ describe DbdOnto::Context do
 
     let(:visibility_subject) do
       subject.detect do |fact|
-        fact.predicate == 'meta:defines_predicate' &&
-        fact.object == 'context:visibility'
+        begin
+          fact.predicate == 'meta:defines_predicate' &&
+          fact.object == 'context:visibility'
+        end
       end.subject
     end
 
     let(:encryption_subject) do
       subject.detect do |fact|
-        fact.predicate == 'meta:defines_predicate' &&
-        fact.object == 'context:encryption'
+        begin
+          fact.predicate == 'meta:defines_predicate' &&
+          fact.object == 'context:encryption'
+        end
       end.subject
     end
 
     let(:source_subject) do
       subject.detect do |fact|
-        fact.predicate == 'meta:defines_predicate' &&
-        fact.object == 'dc:source'
+        begin
+          fact.predicate == 'meta:defines_predicate' &&
+          fact.object == 'dc:source'
+        end
+      end.subject
+    end
+
+    let(:creator_subject) do
+      subject.detect do |fact|
+        begin
+          fact.predicate == 'meta:defines_predicate' &&
+          fact.object == 'dc:creator'
+        end
+      end.subject
+    end
+
+
+    let(:created_subject) do
+      subject.detect do |fact|
+        begin
+          fact.predicate == 'meta:defines_predicate' &&
+          fact.object == 'dcterms:created'
+        end
+      end.subject
+    end
+
+
+    let(:license_subject) do
+      subject.detect do |fact|
+        begin
+          fact.predicate == 'meta:defines_predicate' &&
+          fact.object == 'context:license'
+        end
       end.subject
     end
 
@@ -42,6 +79,25 @@ describe DbdOnto::Context do
       it 'are different subjects' do
         visibility_subject.should_not == source_subject
       end
+    end
+
+    def assert_label_present(resource)
+      resource.detect do |fact|
+        fact.predicate == 'rdfs:label'
+      end.should_not be_nil
+    end
+
+
+    def assert_uri_present(resource)
+      resource.detect do |fact|
+        fact.predicate == 'rdf:uri'
+      end.should_not be_nil
+    end
+
+    def assert_comment_present(resource)
+      resource.detect do |fact|
+        fact.predicate == 'rdfs:comment'
+      end.should_not be_nil
     end
 
     describe 'context:visibility' do
@@ -54,9 +110,19 @@ describe DbdOnto::Context do
 
       it 'has label Context' do
         visibility_facts.detect do |fact|
-          fact.predicate == 'rdfs:label' &&
-          fact.object == 'Visibility'
+          begin
+            fact.predicate == 'rdfs:label' &&
+            fact.object == 'Visibility'
+          end
         end.should_not be_nil
+      end
+
+      it 'has a uri' do
+        assert_uri_present(visibility_facts)
+      end
+
+      it 'has a Comment' do
+        assert_comment_present(visibility_facts)
       end
     end
 
@@ -70,9 +136,19 @@ describe DbdOnto::Context do
 
       it 'has label Encryption' do
         encryption_facts.detect do |fact|
-          fact.predicate == 'rdfs:label' &&
-          fact.object == 'Encryption'
+          begin
+            fact.predicate == 'rdfs:label' &&
+            fact.object == 'Encryption'
+          end
         end.should_not be_nil
+      end
+
+      it 'has a uri' do
+        assert_uri_present(encryption_facts)
+      end
+
+      it 'has a Comment' do
+        assert_comment_present(encryption_facts)
       end
     end
 
@@ -86,36 +162,63 @@ describe DbdOnto::Context do
 
       it 'has label Source' do
         source_facts.detect do |fact|
-          fact.predicate == 'rdfs:label' &&
-          fact.object == 'Source'
+          begin
+            fact.predicate == 'rdfs:label' &&
+            fact.object == 'Source'
+          end
         end.should_not be_nil
       end
+
+      it 'has a uri' do
+        assert_uri_present(source_facts)
+      end
+
+      it 'has a Comment' do
+        assert_comment_present(source_facts)
+      end
+
     end
 
     describe 'dc:creator' do
+
+      let(:creator_facts) { subject.by_subject(creator_subject) }
+
       it 'defines the predicate' do
-        subject.detect do |fact|
-          fact.predicate == 'meta:defines_predicate' &&
-          fact.object == 'dc:creator'
-        end.should_not be_nil
+        creator_subject.should_not be_nil
+      end
+
+      it 'has a uri' do
+        assert_uri_present(creator_facts)
+      end
+
+      it 'has a Comment' do
+        assert_comment_present(creator_facts)
       end
     end
 
     describe 'dcterms:created' do
-      it 'defines the predicate' do
-        subject.detect do |fact|
-          fact.predicate == 'meta:defines_predicate' &&
-          fact.object == 'dcterms:created'
-        end.should_not be_nil
+
+      let(:created_facts) { subject.by_subject(created_subject) }
+
+      it 'has a uri' do
+        assert_uri_present(created_facts)
+      end
+
+      it 'has a Comment' do
+        assert_comment_present(created_facts)
       end
     end
 
     describe 'context:license' do
-      it 'defines the predicate' do
-        subject.detect do |fact|
-          fact.predicate == 'meta:defines_predicate' &&
-          fact.object == 'context:license'
-        end.should_not be_nil
+
+      let(:license_facts) { subject.by_subject(license_subject) }
+
+      it 'has a uri' do
+        assert_uri_present(license_facts)
+      end
+
+      it 'has a Comment' do
+        assert_comment_present(license_facts)
       end
     end
   end
